9c2c9b4e93c18f04254b898b720b19dc4160846d
William Castellano <williamc246@gmail.com>
2017-06-18 21:30:59 -0700

    testing

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const request = require("request");
 
-request({
+/*request({
   uri: "https://rocky-lowlands-48296.herokuapp.com/cars/",
   method: "GET"
 }, function(error, response, body) {
   console.log(body);
-});
+});*/
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello Express World!');
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
