6235bec1f225bc989910e958b639049e5dfb583a
William Castellano <williamc246@gmail.com>
2017-06-18 21:47:33 -0700

    changed listening port to 3000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
 request({
   uri: "https://rocky-lowlands-48296.herokuapp.com/cars/",
