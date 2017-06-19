c8b9b23c5647845ea5ba9511ffd9072c9bc3291c
William Castellano <williamc246@gmail.com>
2017-06-18 22:42:37 -0700

    added pug template engine for view

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
+var carData;
+
 app.set('port', (process.env.PORT || 3000));
+app.set('view engine', 'pug');
 
 request({
   uri: "https://rocky-lowlands-48296.herokuapp.com/cars/",
   method: "GET"
 }, function(error, response, body) {
   console.log(body);
+  carData = body;
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello Express World!');
+  //res.send('Hello Express World!');
+  console.log("*** car data: ", carData);
+  res.render('index', { title: 'Hey', message: 'Hello there!', carData: carData });
 })
 
 app.listen(app.get('port'), function () {
